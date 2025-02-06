a4bad2fa8ab65a761c0beddf1504d68b096e721e
test <test@test.com>
2025-02-06 12:15:39 +0800

    update

---
+++ config.js
@@ -0,0 +1,160 @@
+var config = {
+    "urls": {
+        "msn": [
+            {
+                "url": "https://assets.msn.com/service/MSN/Feed/me?$top=20&DisableTypeSerialization=true&activityId=7E2F0C44-D701-4998-A932-A31D48A50A60&apikey=0QfOX3Vn51YCzitbLaRkTTBadtWpgTN8NZLW0C1SEM&contentType=video&location=21.3744|110.248&market=en-us&query=news%20video&queryType=myfeed&responseSchema=cardview&timeOut=1000&wrapodata=false",
+                "enable": 0
+            },
+            {
+                "url": "https://assets.msn.com/service/MSN/Feed/me?$top=20&DisableTypeSerialization=true&activityId=091A2B0A-F301-4422-A51F-5086F77B0BF3&apikey=0QfOX3Vn51YCzitbLaRkTTBadtWpgTN8NZLW0C1SEM&ocation=21.3744|110.248&market=en-us&query=money%20video&queryType=myfeed&responseSchema=cardview?&timeOut=1000&wrapodata=false",
+                "enable": 0
+            }
+        ],
+        "cnn": [
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/business/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/health/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/politics/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/tech/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/world/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/economy/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/us/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/video/data/3.0/video/uk/relateds.xml",
+                "enable": 0
+            },
+            {
+                "url": "https://edition.cnn.com/playlist/top-news-videos/index.json",
+                "enable": 0
+            }
+        ],
+        "cbs": [
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/top-stories-for-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/full-episodes-auto-vod/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/taboola-around-the-web-video-door/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/ott-vod-main/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/cbs-village-vod-latest-ott-video-door/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/coronavirus-vod-ott-schedule/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/ott-playlists/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/cbs-reports-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/cbsn-where-to-watch/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/48-hours-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/60-minutes-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/cbs-this-morning-branded/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/evening-news-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/60-minutes-overtime-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/america-decides-segments-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/show-the-uplift-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/ott-cbs-saturday-morning/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/sunday-morning-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/face-the-nation-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/the-takeout-ott/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/tbd-media/",
+                "enable": 0
+            },
+            {
+                "url": "https://www.cbsnews.com/video/xhr/collection/component/emergency-component-cbsn/",
+                "enable": 0
+            }
+        ]
+    },
+    "rsss": [
+        {
+            "url": "https://rss.app/feeds/6Ys97t1FKhqXJTDm.xml",
+            "name": "World",
+            "enable": 1
+        },
+        {
+            "url": "https://rss.app/feeds/fdqWRVLYjzbwF7ro.xml",
+            "name": "Money",
+            "enable": 1
+        },
+        {
+            "url": "https://rss.app/feeds/RQ1CZIenvDNfy76G.xml",
+            "name": "Politics ",
+            "enable": 1
+        }
+    ]
+};
+applyConfig(config);
